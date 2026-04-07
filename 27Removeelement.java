06a7c07afcbd391e68ba27a3ff1a521ca30c619a
Arus Rangra <arushrangra@gmail.com>
2026-04-07 23:11:59 +0530

    Create 27Removeelement.java

---
+++ 27Removeelement.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0; // position for valid elements
+
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
